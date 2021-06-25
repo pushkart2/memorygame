c6d7cfa67ac5f8beb2494bae1552e469cda45c34
pushkart2 <pushkart2@gmail.com>
2021-06-25 08:36:57 -0500

    Added all files

---
+++ client/main.lua
@@ -0,0 +1,48 @@
+
+local successCb
+local failCb
+local resultReceived = false
+
+RegisterNUICallback('ThermiteResult', function(data, cb)
+    -- exports['exo-inventory']:ToggleHotBar(true) Toggle so that inventory doesnt work.. this was something I made on my own server.. 
+    SetNuiFocus(false, false)
+    resultReceived = true
+    if data.success then
+        successCb()
+    else
+        failCb()
+    end
+    cb('ok')
+    TriggerEvent('progressbar:client:ToggleBusyness', false)
+end)
+
+RegisterCommand('thermite', function(source, args)
+    -- Please check the parameters below for exports
+    exports["memorygame"]:thermiteminigame(10, 3, 3, 10,
+    function() -- success
+        print("success")
+    end,
+    function() -- failure
+        print("failure")
+    end)
+end)
+
+exports('thermiteminigame', function(correctBlocks, incorrectBlocks, timetoShow, timetoLose, success, fail)
+    -- correctBlocks = Number of correct blocks the player needs to click
+    -- incorrectBlocks = number of incorrect blocks after which the game will fail
+    -- timetoShow = time in secs for which the right blocks will be shown
+    -- timetoLose = maximum time after timetoshow expires for player to select the right blocks
+    resultReceived = false
+    successCb = success
+    failCb = fail
+    -- exports['exo-inventory']:ToggleHotBar(false) Toggle so that inventory doesnt work.. this was something I made on my own server.. 
+    SetNuiFocus(true, true)
+    SendNUIMessage({
+        action = "Start",
+        correct = correctBlocks,
+        incorrect = incorrectBlocks,
+        showtime = timetoShow,
+        losetime = timetoLose + timetoShow,
+    })
+    TriggerEvent('progressbar:client:ToggleBusyness', true)
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'adamant'
+
+game 'gta5'
+author 'Snipe'
+description 'Thermite Game Like NoPixel'
+version '1.0.0'
+server_scripts {
+  --'server/server.lua',
+}
+
+client_scripts {
+  'client/main.lua',
+}
+
+ui_page {
+  'html/index.html'
+}
+
+files {
+  'html/index.html',
+  'html/style.css',
+  'html/app.js'
+}
